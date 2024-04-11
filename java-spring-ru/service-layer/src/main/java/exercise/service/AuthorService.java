973d4060572dc36aa1c8bc862e22f0da92e9e5e3
@hexlet/cli <support@hexlet.io>
2024-04-11 07:39:44 +0300

    submit java-spring-ru/service-layer

---
+++ java-spring-ru/service-layer/src/main/java/exercise/service/AuthorService.java
@@ -0,0 +1,39 @@
+package exercise.service;
+
+import exercise.dto.AuthorCreateDTO;
+import exercise.dto.AuthorDTO;
+import exercise.dto.AuthorUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
+import exercise.mapper.AuthorMapper;
+import exercise.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+    // BEGIN
+    @Autowired
+    private AuthorRepository repository;
+
+    @Autowired
+    private AuthorMapper authorMapper;
+
+    public List<AuthorDTO> getAll() {
+        var authors = repository.findAll();
+        var result = authors.stream()
+                .map(authorMapper::map)
+                .toList();
+        return result;
+    }
+
+    public AuthorDTO create(AuthorCreateDTO authorData) {
+        var author = authorMapper.map(authorData);
+//        author.setAuthor(authorUtils.getCurrentAuthor());
+        repository.save(author);
+        var authorDTO = authorMapper.map(author);
+        return authorDTO;
+    }
+    // END
+}
